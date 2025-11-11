3801cddd52df724cabd00a2f86d64ec10651f05c
Daffakusumaa <122962373+Daffakusumaa@users.noreply.github.com>
2025-11-11 22:44:47 +0700

    Added Ultimate Mechanic & bug fixes
    - Added Ultimate that swing the sword 360
    - Added 2 Type of Enemies (Blue Virus & Green Virus)
    - Fixed Character cant flip after using ultimate

--- Assets/Dappa/Scripts/Player/PlayerController.cs
+++ Assets/Dappa/Scripts/Player/PlayerController.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; }}
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
+    public static PlayerController Instance;
     [SerializeField] private float moveSpeed = 1f;
 
     private PlayerControls playerControls;
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
